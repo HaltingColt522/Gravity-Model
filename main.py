077af0a19bc21cc2517ad9d70e5703b60358c793
HaltingColt522 <l.frotschi@web.de>
2024-02-28 12:53:00 +0100

    a little bit

--- main.py
+++ main.py
@@ -1,6 +1,71 @@
 import pygame
+import sys
+from PySide6 import QtCore, QtWidgets as QtW, QtGui
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+import math
 
 pygame.init()
+
+
+class configWindow(QtW.QWidget):
+    def __init__(self, x: int, y: int):
+        super().__init__()
+        self.gravitationalConstance = 6.673 * 10**-11
+
+        self.gravitational_force_label = QtW.QLabel()
+
+        self.MassLabel = QtW.QLabel()
+        self.MassLabel.setText("Mass in kg")
+
+        self.MassInput = QtW.QLineEdit()
+        self.MassInput.setPlaceholderText("Input planet mass")
+        self.Mass = 0
+
+        self.RadiusLabel = QtW.QLabel()
+        self.RadiusLabel.setText("Radius in km")
+
+        self.RadiusInput = QtW.QLineEdit()
+        self.RadiusInput.setPlaceholderText("Input planet radius")
+        self.Radius = 0
+
+        self.Center = x, y
+
+        self.createButton = QtW.QPushButton("Create model")
+        self.createButton.clicked.connect(self.calculate_gravitational_force)
+
+        self.layout = QtW.QVBoxLayout(self)
+        self.layout.addWidget(self.MassLabel)
+        self.layout.addWidget(self.MassInput)
+        self.layout.addWidget(self.RadiusLabel)
+        self.layout.addWidget(self.RadiusInput)
+        self.layout.addWidget(self.createButton)
+        self.layout.addWidget(self.gravitational_force_label)
+
+    def calculate_gravitational_force(self):
+        self.Mass = self.MassInput.text()
+        self.Radius = self.RadiusInput.text()
+        self.gravitational_force = self.gravitationalConstance * (
+            (float(self.Mass)) / (float(self.Radius) ** 2)
+        )
+        self.gravitational_force_label.setText(str(self.gravitational_force))
+
+
+if __name__ == "__main__":
+    app = QtW.QApplication([])
+
+    widget = configWindow(x=400, y=300)
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
+
+
+class planet:
+    def __init__(self):
+        self.Mass: float = 0
+        self.Radius: float = 0
+        self.isInSolarSystem: bool = False
+
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
